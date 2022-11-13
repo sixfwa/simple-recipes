7f0c870853720b5a988571bffed02ce69102bed7
Farhan Ali <sixfwali@gmail.com>
2022-11-13 02:09:19 +0100

    welcome message call

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container fluid>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+
 const HomePage = () => {
-  return <h1>Hello</h1>;
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("/.netlify/functions/welcome")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      });
+  }, []);
+
+  return (
+    <Row>
+      <Col>
+        <h1>Welcome</h1>
+        <p>{message}</p>
+      </Col>
+    </Row>
+  );
 };
 
 export default HomePage;
